9135a244abcf5becf98f530907f774e0d071a829
Ivan Golovinov <topclassprogrammer@gmail.com>
2024-08-17 20:24:46 +0300

    wf13

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-  GNU nano 7.2                                             deploy.sh                                                       
-#!/bin/bash
-cd /opt/cicd_test
-git pull origin main
-source env/bin/activate
-pip install -r requirements.txt
-python webpython/manage.py migrate
-sudo systemctl restart gunicorn
--- webpython/demo/views.py
+++ webpython/demo/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Netology has been started")
+    return HttpResponse("Just Netology")
